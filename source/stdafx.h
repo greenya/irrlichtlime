cf0585b4a8906614c2f0822f5f5fb94ba4ba2356
Yuriy Grynevych <greenyadzer@gmail.com>
2010-05-18 17:21:37 +0000

    Added enums: KeyAction, KeyCode.

--- source/stdafx.h
+++ source/stdafx.h
@@ -6,19 +6,21 @@
 
 #include "Lime.h"
 
-// ::Core types
-#include "Vector.h"
-#include "Dimension.h"
-#include "Rect.h"
-
-// ::Video types
-#include "Color.h"
-
 // Enums
 #include "AnimatedMeshType.h"
 #include "AnimationTypeMD2.h"
 #include "DriverType.h"
 #include "FileArchiveType.h"
 #include "GUIElementType.h"
+#include "KeyAction.h"
+#include "KeyCode.h"
 #include "MaterialFlag.h"
 #include "SceneNodeType.h"
+
+// IrrlichtLime::Core types
+#include "Vector.h"
+#include "Dimension.h"
+#include "Rect.h"
+
+// IrrlichtLime::Video types
+#include "Color.h"
