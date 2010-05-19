8c5d685123c8089cc0c0afe1ae10765d380107dd
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2010-05-19 17:24:52 +0000

    Updated version in AssemblyInfo.cpp.

--- trunk/source/AssemblyInfo.cpp
+++ trunk/source/AssemblyInfo.cpp
@@ -15,7 +15,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("0.0.*")];
+[assembly:AssemblyVersionAttribute("0.1.*")];
 
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
