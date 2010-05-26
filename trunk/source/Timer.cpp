f00b3a4d6ff1271482cdccf64e13b9dcdb5f1008
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2010-05-26 15:07:26 +0000

    Added class Timer. Added property IrrlichtDevice.Timer.

---
+++ trunk/source/Timer.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include "Timer.h"
+
+using namespace irr;
+using namespace System;
+using namespace IrrlichtLime::Core;
+
+namespace IrrlichtLime {
+
+Timer::Timer(irr::ITimer* timer)
+	: ReferenceCounted(timer)
+{
+	LIME_ASSERT(timer != nullptr);
+	m_Timer = timer;
+}
+
+void Timer::Start()
+{
+	m_Timer->start();
+}
+
+void Timer::Stop()
+{
+	m_Timer->stop();
+}
+
+void Timer::Tick()
+{
+	m_Timer->tick();
+}
+
+bool Timer::IsStopped::get()
+{
+	return m_Timer->isStopped();
+}
+
+unsigned int Timer::RealTime::get()
+{
+	return m_Timer->getRealTime();
+}
+
+float Timer::Speed::get()
+{
+	return m_Timer->getSpeed();
+}
+
+void Timer::Speed::set(float value)
+{
+	m_Timer->setSpeed(value);
+}
+
+unsigned int Timer::Time::get()
+{
+	return m_Timer->getTime();
+}
+
+void Timer::Time::set(unsigned int value)
+{
+	m_Timer->setTime(value);
+}
+
+String^ Timer::ToString()
+{
+	unsigned int t = Time;
+
+	unsigned int millisecs = t % 1000; t /= 1000;
+	unsigned int seconds = t % 60; t /= 60;
+	unsigned int minutes = t % 60; t /= 60;
+	unsigned int hours = t;
+
+	return String::Format("{0}:{1:00}:{2:00}.{3:000}", hours, minutes, seconds, millisecs);
+}
+
+} // end namespace IrrlichtLime
