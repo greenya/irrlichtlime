66bf6df4341e5b2941e46b6959843770608c05ea
Yuriy Grynevych <greenyadzer@gmail.com>
2010-05-31 23:44:40 +0000

    GUIStaticText extended with methods: SetBackgroundColor, SetDrawBackground, SetDrawBorder, SetTextAlignment; props: OverrideColor, OverrideColorEnabled, OverrideFont, TextHeight, TextWidth, WordWrap.

--- source/GUIStaticText.cpp
+++ source/GUIStaticText.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "GUIElement.h"
+#include "GUIFont.h"
 #include "GUIStaticText.h"
 
 using namespace irr;
@@ -13,6 +14,78 @@ GUIStaticText::GUIStaticText(gui::IGUIStaticText* guiStaticText)
 {
 	LIME_ASSERT(guiStaticText != nullptr);
 	m_GUIStaticText = guiStaticText;
+}
+
+void GUIStaticText::SetBackgroundColor(Video::Coloru^ color)
+{
+	LIME_ASSERT(color != nullptr);
+	m_GUIStaticText->setBackgroundColor(*color->m_NativeValue);
+}
+
+void GUIStaticText::SetDrawBackground(bool draw)
+{
+	m_GUIStaticText->setDrawBackground(draw);
+}
+
+void GUIStaticText::SetDrawBorder(bool draw)
+{
+	m_GUIStaticText->setDrawBorder(draw);
+}
+
+void GUIStaticText::SetTextAlignment(GUIAlignment horizontal, GUIAlignment vertical)
+{
+	m_GUIStaticText->setTextAlignment((gui::EGUI_ALIGNMENT)horizontal, (gui::EGUI_ALIGNMENT)vertical);
+}
+
+Video::Coloru^ GUIStaticText::OverrideColor::get()
+{
+	return gcnew Video::Coloru(m_GUIStaticText->getOverrideColor());
+}
+
+void GUIStaticText::OverrideColor::set(Video::Coloru^ value)
+{
+	LIME_ASSERT(value != nullptr);
+	m_GUIStaticText->setOverrideColor(*value->m_NativeValue);
+}
+
+bool GUIStaticText::OverrideColorEnabled::get()
+{
+	return m_GUIStaticText->isOverrideColorEnabled();
+}
+
+void GUIStaticText::OverrideColorEnabled::set(bool value)
+{
+	m_GUIStaticText->enableOverrideColor(value);
+}
+
+GUIFont^ GUIStaticText::OverrideFont::get()
+{
+	return LIME_SAFEWRAP(GUIFont, m_GUIStaticText->getOverrideFont());
+}
+
+void GUIStaticText::OverrideFont::set(GUIFont^ value)
+{
+	m_GUIStaticText->setOverrideFont(LIME_SAFEREF(value, m_GUIFont));
+}
+
+int GUIStaticText::TextHeight::get()
+{
+	return m_GUIStaticText->getTextHeight();
+}
+
+int GUIStaticText::TextWidth::get()
+{
+	return m_GUIStaticText->getTextWidth();
+}
+
+bool GUIStaticText::WordWrap::get()
+{
+	return m_GUIStaticText->isWordWrapEnabled();
+}
+
+void GUIStaticText::WordWrap::set(bool value)
+{
+	m_GUIStaticText->setWordWrap(value);
 }
 
 } // end namespace GUI
--- source/GUIStaticText.h
+++ source/GUIStaticText.h
@@ -9,9 +9,23 @@ using namespace System;
 namespace IrrlichtLime {
 namespace GUI {
 
+ref class GUIFont;
+
 public ref class GUIStaticText : GUIElement
 {
 public:
+
+	void SetBackgroundColor(Video::Coloru^ color);
+	void SetDrawBackground(bool draw);
+	void SetDrawBorder(bool draw);
+	void SetTextAlignment(GUIAlignment horizontal, GUIAlignment vertical);
+
+	property Video::Coloru^ OverrideColor { Video::Coloru^ get(); void set(Video::Coloru^ value); }
+	property bool OverrideColorEnabled { bool get(); void set(bool value); }
+	property GUIFont^ OverrideFont { GUIFont^ get(); void set(GUIFont^ value); }
+	property int TextHeight { int get(); }
+	property int TextWidth { int get(); }
+	property bool WordWrap { bool get(); void set(bool value); }
 
 internal:
 
