428d81e05c5f423df7291b4d381f40417074de9c
Yuriy Grynevych <greenyadzer@gmail.com>
2010-06-05 12:14:07 +0000

    Updated changes.txt. Updated AssemblyInfo version to 0.3.

--- source/AssemblyInfo.cpp
+++ source/AssemblyInfo.cpp
@@ -15,7 +15,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("0.2.*")];
+[assembly:AssemblyVersionAttribute("0.3.*")];
 
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
