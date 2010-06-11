c218a56574975925f64b37a848fe45f6240b76de
Yuriy Grynevych <greenyadzer@gmail.com>
2010-06-11 12:48:46 +0000

    Vector2D improved; now available int and float versions.

--- source/Vector.h
+++ source/Vector.h
@@ -12,115 +12,21 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Core {
 
-public ref class Vector2Df : Lime::NativeValue<core::vector2df>
-{
-public:
+#define _REFCLASS_ Vector2Df
+#define _WRAPCLASS_ core::vector2df
+#define _WRAPTYPE_ float
+#include "Vector2D_template.h"
+#undef _WRAPTYPE_
+#undef _WRAPCLASS_
+#undef _REFCLASS_
 
-	Vector2Df(Vector2Df^ copy)
-		: Lime::NativeValue<core::vector2df>(true)
-	{
-		LIME_ASSERT(copy != nullptr);
-		m_NativeValue = new core::vector2df(*copy->m_NativeValue);
-	}
-
-	Vector2Df(float x, float y)
-		: Lime::NativeValue<core::vector2df>(true)
-	{
-		m_NativeValue = new core::vector2df(x, y);
-	}
-
-	Vector2Df(float xy)
-		: Lime::NativeValue<core::vector2df>(true)
-	{
-		m_NativeValue = new core::vector2df(xy);
-	}
-
-	Vector2Df()
-		: Lime::NativeValue<core::vector2df>(true)
-	{
-		m_NativeValue = new core::vector2df();
-	}
-
-	property float X
-	{
-		float get() { return m_NativeValue->X; }
-		void set(float value) { m_NativeValue->X = value; }
-	}
-
-	property float Y
-	{
-		float get() { return m_NativeValue->Y; }
-		void set(float value) { m_NativeValue->Y = value; }
-	}
-
-	virtual String^ ToString() override
-	{
-		return String::Format("{0},{1}", X, Y);
-	}
-
-internal:
-
-	Vector2Df(const core::vector2df& other)
-		: Lime::NativeValue<core::vector2df>(true)
-	{
-		m_NativeValue = new core::vector2df(other);
-	}
-};
-
-public ref class Vector2Di : Lime::NativeValue<core::vector2di>
-{
-public:
-
-	Vector2Di(Vector2Di^ copy)
-		: Lime::NativeValue<core::vector2di>(true)
-	{
-		LIME_ASSERT(copy != nullptr);
-		m_NativeValue = new core::vector2di(*copy->m_NativeValue);
-	}
-
-	Vector2Di(int x, int y)
-		: Lime::NativeValue<core::vector2di>(true)
-	{
-		m_NativeValue = new core::vector2di(x, y);
-	}
-
-	Vector2Di(int xy)
-		: Lime::NativeValue<core::vector2di>(true)
-	{
-		m_NativeValue = new core::vector2di(xy);
-	}
-
-	Vector2Di()
-		: Lime::NativeValue<core::vector2di>(true)
-	{
-		m_NativeValue = new core::vector2di();
-	}
-
-	property int X
-	{
-		int get() { return m_NativeValue->X; }
-		void set(int value) { m_NativeValue->X = value; }
-	}
-
-	property int Y
-	{
-		int get() { return m_NativeValue->Y; }
-		void set(int value) { m_NativeValue->Y = value; }
-	}
-
-	virtual String^ ToString() override
-	{
-		return String::Format("{0},{1}", X, Y);
-	}
-
-internal:
-
-	Vector2Di(const core::vector2di& other)
-		: Lime::NativeValue<core::vector2di>(true)
-	{
-		m_NativeValue = new core::vector2di(other);
-	}
-};
+#define _REFCLASS_ Vector2Di
+#define _WRAPCLASS_ core::vector2di
+#define _WRAPTYPE_ int
+#include "Vector2D_template.h"
+#undef _WRAPTYPE_
+#undef _WRAPCLASS_
+#undef _REFCLASS_
 
 #define _REFCLASS_ Vector3Df
 #define _WRAPCLASS_ core::vector3df
---
+++ source/Vector2D_template.h
@@ -0,0 +1,54 @@
+#if !defined(_REFCLASS_) || !defined(_WRAPCLASS_) || !defined(_WRAPTYPE_)
+#error _REFCLASS_, _WRAPCLASS_ and _WRAPTYPE_ must be defined for this file to process.
+#endif
+
+public ref class _REFCLASS_ : Lime::NativeValue<_WRAPCLASS_>
+{
+	#include "Vector_common_template.h"
+
+public:
+
+	_REFCLASS_(_WRAPTYPE_ x, _WRAPTYPE_ y)
+		: Lime::NativeValue<_WRAPCLASS_>(true)
+	{
+		m_NativeValue = new _WRAPCLASS_(x, y);
+	}
+
+	void Set(_WRAPTYPE_ x, _WRAPTYPE_ y)
+	{
+		m_NativeValue->set(x, y);
+	}
+
+	double GetAngleWith(_REFCLASS_^ other)
+	{
+		LIME_ASSERT(other != nullptr);
+		return m_NativeValue->getAngleWith(*other->m_NativeValue);
+	}
+
+	_REFCLASS_^ RotateBy(double degrees, _REFCLASS_^ center)
+	{
+		LIME_ASSERT(center != nullptr);
+		return gcnew _REFCLASS_(m_NativeValue->rotateBy(degrees, *center->m_NativeValue));
+	}
+
+	_REFCLASS_^ RotateBy(double degrees)
+	{
+		return gcnew _REFCLASS_(m_NativeValue->rotateBy(degrees));
+	}
+
+	property double Angle
+	{
+		double get() { return m_NativeValue->getAngle(); }
+	}
+
+	//excluded becuase of http://irrlicht.sourceforge.net/phpBB2/viewtopic.php?p=225930#225930
+	//property double AngleTrig
+	//{
+	//	double get() { return m_NativeValue->getAngleTrig(); }
+	//}
+
+	virtual String^ ToString() override
+	{
+		return String::Format("{0},{1}", X, Y);
+	}
+};
