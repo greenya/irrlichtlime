01f24c8ec6ee5034eea6931ad811d34da7e891da
Yuriy Grynevych <greenyadzer@gmail.com>
2010-06-16 12:35:20 +0000

    Added classes ParticleAnimatedMeshSceneNodeEmitter, ParticleAttractionAffector, ParticleBoxEmitter, ParticleCylinderEmitter, ParticleFadeOutAffector, ParticleGravityAffector. Class ParticleSystemSceneNode extended with methods: CreateAnimatedMeshSceneNodeEmitter, CreateAttractionAffector, CreateBoxEmitter, CreateCylinderEmitter, CreateFadeOutParticleAffector and CreateGravityAffector.

---
+++ source/ParticleFadeOutAffector.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "ParticleAffector.h"
+#include "ParticleFadeOutAffector.h"
+
+using namespace irr;
+using namespace System;
+using namespace IrrlichtLime::Core;
+
+namespace IrrlichtLime {
+namespace Scene {
+
+ParticleFadeOutAffector^ ParticleFadeOutAffector::Wrap(scene::IParticleFadeOutAffector* ref)
+{
+	if (ref == nullptr)
+		return nullptr;
+
+	return gcnew ParticleFadeOutAffector(ref);
+}
+
+ParticleFadeOutAffector::ParticleFadeOutAffector(scene::IParticleFadeOutAffector* ref)
+	: ParticleAffector(ref)
+{
+	LIME_ASSERT(ref != nullptr);
+	m_ParticleFadeOutAffector = ref;
+}
+
+float ParticleFadeOutAffector::FadeOutTime::get()
+{
+	return m_ParticleFadeOutAffector->getFadeOutTime();
+}
+
+void ParticleFadeOutAffector::FadeOutTime::set(float value)
+{
+	LIME_ASSERT(value >= 0.0f);
+	m_ParticleFadeOutAffector->setFadeOutTime(value);
+}
+
+Video::Coloru^ ParticleFadeOutAffector::TargetColor::get()
+{
+	return gcnew Video::Coloru(m_ParticleFadeOutAffector->getTargetColor());
+}
+
+void ParticleFadeOutAffector::TargetColor::set(Video::Coloru^ value)
+{
+	LIME_ASSERT(value != nullptr);
+	m_ParticleFadeOutAffector->setTargetColor(*value->m_NativeValue);
+}
+
+} // end namespace Scene
+} // end namespace IrrlichtLime
