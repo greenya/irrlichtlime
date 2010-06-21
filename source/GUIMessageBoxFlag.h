4aca6e631febcfc6bf2fa193724d4dd83bc34633
Yuriy Grynevych <greenyadzer@gmail.com>
2010-06-21 16:17:50 +0000

    Added enums FogType, GUIMessageBoxFlag and TextureCreationFlag. Added class Fog for holding fog data. Class VideoDriver extended with methods SetMinHardwareBufferVertexCount, SetTextureCreationFlag, TurnLight and WriteImageToFile; prop Fog.

---
+++ source/GUIMessageBoxFlag.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "stdafx.h"
+
+using namespace irr;
+using namespace gui;
+using namespace System;
+
+namespace IrrlichtLime {
+namespace GUI {
+
+[Flags]
+public enum class GUIMessageBoxFlag : unsigned int
+{
+	OK = EMBF_OK,
+	Cancel = EMBF_CANCEL,
+	Yes = EMBF_YES,
+	No = EMBF_NO
+};
+
+} // end namespace GUI
+} // end namespace IrrlichtLime
