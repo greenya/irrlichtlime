02e6944754426f81e602cf951b0e8f858ceb1bd0
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2010-06-29 14:41:58 +0000

    Added ability to compile Lime in Release mode. Added release version of Irrlicht.dll. Made proper changes to proj files. Updated AssemblyInfo' AssemblyDescriptionAttribute to indicate debug build.

--- trunk/source/AssemblyInfo.cpp
+++ trunk/source/AssemblyInfo.cpp
@@ -7,7 +7,13 @@ using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
 [assembly:AssemblyTitleAttribute("Irrlicht Lime")];
+
+#if _DEBUG
+[assembly:AssemblyDescriptionAttribute("The Irrlicht Lime is a .NET wrapper for the Irrlicht Engine. (DEBUG)")];
+#else
 [assembly:AssemblyDescriptionAttribute("The Irrlicht Lime is a .NET wrapper for the Irrlicht Engine.")];
+#endif
+
 [assembly:AssemblyConfigurationAttribute("")];
 [assembly:AssemblyCompanyAttribute("")];
 [assembly:AssemblyProductAttribute("Irrlicht Lime")];
