a6821ed27c903ccca2007bee8f896f0d8ae2f1ef
Yuriy Grynevych <greenyadzer@gmail.com>
2010-07-02 16:12:09 +0000

    Added example 11.PerPixelLighting and media for it.

--- source/Matrix.h
+++ source/Matrix.h
@@ -18,6 +18,12 @@ public:
 		m_NativeValue = new core::matrix4();
 	}
 
+	property Vector3Df^ Scale
+	{
+		Vector3Df^ get() { return gcnew Vector3Df(m_NativeValue->getScale()); }
+		void set(Vector3Df^ value) { LIME_ASSERT(value != nullptr); m_NativeValue->setScale(*value->m_NativeValue); }
+	}
+
 internal:
 
 	Matrix(const core::matrix4& other)
