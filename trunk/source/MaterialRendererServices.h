6343fdc8cdb30fb13fa0751d36886cc40f65f0ea
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2010-07-26 13:58:28 +0000

    Added class MaterialRendererServices.

---
+++ trunk/source/MaterialRendererServices.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "stdafx.h"
+
+using namespace irr;
+using namespace System;
+using namespace IrrlichtLime::Core;
+
+namespace IrrlichtLime {
+namespace Video {
+
+ref class Material;
+ref class VideoDriver;
+
+public ref class MaterialRendererServices
+{
+public:
+
+	void SetBasicRenderStates(Material^ newMaterial, Material^ lastMaterial, bool resetAllRenderStates);
+
+	void SetPixelShaderRegister(int registerIndex, array<float>^ value);
+	void SetPixelShaderRegisters(int startRegisterIndex, array<float>^ values);
+	bool SetPixelShaderVariable(String^ name, array<float>^ values);
+
+	void SetVertexShaderRegister(int registerIndex, array<float>^ value);
+	void SetVertexShaderRegisters(int startRegisterIndex, array<float>^ values);
+	bool SetVertexShaderVariable(String^ name, array<float>^ values);
+
+	property Video::VideoDriver^ VideoDriver { Video::VideoDriver^ get(); }
+
+	virtual String^ ToString() override;
+
+internal:
+
+	static MaterialRendererServices^ Wrap(video::IMaterialRendererServices* ref);
+	MaterialRendererServices(video::IMaterialRendererServices* ref);
+
+	video::IMaterialRendererServices* m_MaterialRendererServices;
+};
+
+} // end namespace Video
+} // end namespace IrrlichtLime
