c6490f4ffb10a02be98eb24b71551927c4a5e950
Yuriy Grynevych <greenyadzer@gmail.com>
2010-09-16 12:02:44 +0000

    Added additional (shorter versions) constructors to Vertex3D. Class MeshBuffer extended with methods: UpdateIndices, UpdateVertices and 2 overloadings of Append.

--- source/MeshBuffer.cpp
+++ source/MeshBuffer.cpp
@@ -37,12 +37,57 @@ void MeshBuffer::Append(array<Video::Vertex3D^>^ vertices, array<unsigned short>
 	LIME_ASSERT(indices != nullptr);
 
 	int vc = vertices->Length;
-	int ic = indices->Length;
-
 	video::S3DVertex* va = new video::S3DVertex[vc];
 	for (int i = 0; i < vc; i++)
 		va[i] = *vertices[i]->m_NativeValue;
 
+	int ic = indices->Length;
+	unsigned short* ia = new unsigned short[ic];
+	for (int i = 0; i < ic; i++)
+		ia[i] = indices[i];
+
+	m_MeshBuffer->append(va, vc, ia, ic);
+
+	delete ia;
+	delete va;
+}
+
+void MeshBuffer::Append(array<Video::Vertex3DTTCoords^>^ vertices, array<unsigned short>^ indices)
+{
+	LIME_ASSERT(this->VertexType == Video::VertexType::TTCoords);
+	LIME_ASSERT(this->IndexType == Video::IndexType::_16Bit);
+	LIME_ASSERT(vertices != nullptr);
+	LIME_ASSERT(indices != nullptr);
+
+	int vc = vertices->Length;
+	video::S3DVertex* va = new video::S3DVertex2TCoords[vc];
+	for (int i = 0; i < vc; i++)
+		va[i] = *vertices[i]->m_NativeValue;
+
+	int ic = indices->Length;
+	unsigned short* ia = new unsigned short[ic];
+	for (int i = 0; i < ic; i++)
+		ia[i] = indices[i];
+
+	m_MeshBuffer->append(va, vc, ia, ic);
+
+	delete ia;
+	delete va;
+}
+
+void MeshBuffer::Append(array<Video::Vertex3DTangents^>^ vertices, array<unsigned short>^ indices)
+{
+	LIME_ASSERT(this->VertexType == Video::VertexType::Tangents);
+	LIME_ASSERT(this->IndexType == Video::IndexType::_16Bit);
+	LIME_ASSERT(vertices != nullptr);
+	LIME_ASSERT(indices != nullptr);
+
+	int vc = vertices->Length;
+	video::S3DVertex* va = new video::S3DVertexTangents[vc];
+	for (int i = 0; i < vc; i++)
+		va[i] = *vertices[i]->m_NativeValue;
+
+	int ic = indices->Length;
 	unsigned short* ia = new unsigned short[ic];
 	for (int i = 0; i < ic; i++)
 		ia[i] = indices[i];
@@ -84,6 +129,54 @@ void MeshBuffer::SetDirty(HardwareBufferType buffer)
 void MeshBuffer::SetHardwareMappingHint(HardwareMappingHint mappingHint, HardwareBufferType buffer)
 {
 	m_MeshBuffer->setHardwareMappingHint((scene::E_HARDWARE_MAPPING)mappingHint, (scene::E_BUFFER_TYPE)buffer);
+}
+
+void MeshBuffer::UpdateIndices(array<unsigned short>^ indices, int startIndex)
+{
+	LIME_ASSERT(this->IndexType == Video::IndexType::_16Bit);
+	LIME_ASSERT(indices != nullptr);
+	LIME_ASSERT(startIndex + indices->Length <= this->IndexCount);
+
+	unsigned short* p = m_MeshBuffer->getIndices();
+
+	for (int i = 0; i < indices->Length; i++)
+		p[i + startIndex] = indices[i];
+}
+
+void MeshBuffer::UpdateVertices(array<Video::Vertex3D^>^ vertices, int startIndex)
+{
+	LIME_ASSERT(this->VertexType == Video::VertexType::Standard);
+	LIME_ASSERT(vertices != nullptr);
+	LIME_ASSERT(startIndex + vertices->Length <= this->VertexCount);
+
+	video::S3DVertex* p = (video::S3DVertex*)m_MeshBuffer->getVertices();
+
+	for (int i = 0; i < vertices->Length; i++)
+		p[i + startIndex] = *vertices[i]->m_NativeValue;
+}
+
+void MeshBuffer::UpdateVertices(array<Video::Vertex3DTTCoords^>^ vertices, int startIndex)
+{
+	LIME_ASSERT(this->VertexType == Video::VertexType::TTCoords);
+	LIME_ASSERT(vertices != nullptr);
+	LIME_ASSERT(startIndex + vertices->Length <= this->VertexCount);
+
+	video::S3DVertex2TCoords* p = (video::S3DVertex2TCoords*)m_MeshBuffer->getVertices();
+
+	for (int i = 0; i < vertices->Length; i++)
+		p[i + startIndex] = *vertices[i]->m_NativeValue;
+}
+
+void MeshBuffer::UpdateVertices(array<Video::Vertex3DTangents^>^ vertices, int startIndex)
+{
+	LIME_ASSERT(this->VertexType == Video::VertexType::Tangents);
+	LIME_ASSERT(vertices != nullptr);
+	LIME_ASSERT(startIndex + vertices->Length <= this->VertexCount);
+
+	video::S3DVertexTangents* p = (video::S3DVertexTangents*)m_MeshBuffer->getVertices();
+
+	for (int i = 0; i < vertices->Length; i++)
+		p[i + startIndex] = *vertices[i]->m_NativeValue;
 }
 
 AABBox^ MeshBuffer::BoundingBox::get()
