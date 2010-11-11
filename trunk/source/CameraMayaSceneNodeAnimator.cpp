c87f59d74e3f024b82114a8bdfb4dc54edf8ed6c
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2010-11-11 15:03:05 +0000

    Added CameraMayaSceneNodeAnimator class.

---
+++ trunk/source/CameraMayaSceneNodeAnimator.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "CameraMayaSceneNodeAnimator.h"
+
+using namespace irr;
+using namespace System;
+
+namespace IrrlichtLime {
+namespace Scene {
+
+CameraMayaSceneNodeAnimator^ CameraMayaSceneNodeAnimator::Wrap(scene::ISceneNodeAnimatorCameraMaya* ref)
+{
+	if (ref == nullptr)
+		return nullptr;
+
+	return gcnew CameraMayaSceneNodeAnimator(ref);
+}
+
+CameraMayaSceneNodeAnimator::CameraMayaSceneNodeAnimator(scene::ISceneNodeAnimatorCameraMaya* ref)
+	: SceneNodeAnimator(ref)
+{
+	LIME_ASSERT(ref != nullptr);
+	m_CameraMayaSceneNodeAnimator = ref;
+}
+
+float CameraMayaSceneNodeAnimator::MoveSpeed::get()
+{
+	return m_CameraMayaSceneNodeAnimator->getMoveSpeed();
+}
+
+void CameraMayaSceneNodeAnimator::MoveSpeed::set(float value)
+{
+	m_CameraMayaSceneNodeAnimator->setMoveSpeed(value);
+}
+
+float CameraMayaSceneNodeAnimator::RotateSpeed::get()
+{
+	return m_CameraMayaSceneNodeAnimator->getRotateSpeed();
+}
+
+void CameraMayaSceneNodeAnimator::RotateSpeed::set(float value)
+{
+	m_CameraMayaSceneNodeAnimator->setRotateSpeed(value);
+}
+
+float CameraMayaSceneNodeAnimator::ZoomSpeed::get()
+{
+	return m_CameraMayaSceneNodeAnimator->getZoomSpeed();
+}
+
+void CameraMayaSceneNodeAnimator::ZoomSpeed::set(float value)
+{
+	m_CameraMayaSceneNodeAnimator->setZoomSpeed(value);
+}
+
+} // end namespace Scene
+} // end namespace IrrlichtLime
