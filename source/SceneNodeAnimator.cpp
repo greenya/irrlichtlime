7f9e4f123c1e42976b82dddab01bfef4989ac9f4
Yuriy Grynevych <greenyadzer@gmail.com>
2010-11-11 15:05:23 +0000

    Added correct type solving for all derived classes from SceneNodeAnimator.

--- source/SceneNodeAnimator.cpp
+++ source/SceneNodeAnimator.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "AttributeExchangingObject.h"
+#include "CameraFPSSceneNodeAnimator.h"
+#include "CameraMayaSceneNodeAnimator.h"
+#include "CollisionResponseSceneNodeAnimator.h"
 #include "SceneNode.h"
 #include "SceneNodeAnimator.h"
 
@@ -14,7 +17,26 @@ SceneNodeAnimator^ SceneNodeAnimator::Wrap(scene::ISceneNodeAnimator* ref)
 	if (ref == nullptr)
 		return nullptr;
 
-	return gcnew SceneNodeAnimator(ref);
+	switch (ref->getType())
+	{
+	case scene::ESNAT_COLLISION_RESPONSE:
+		return gcnew CollisionResponseSceneNodeAnimator((scene::ISceneNodeAnimatorCollisionResponse*)ref);
+
+	case scene::ESNAT_CAMERA_FPS:
+		return gcnew CameraFPSSceneNodeAnimator((scene::ISceneNodeAnimatorCameraFPS*)ref);
+
+	case scene::ESNAT_CAMERA_MAYA:
+		return gcnew CameraMayaSceneNodeAnimator((scene::ISceneNodeAnimatorCameraMaya*)ref);
+
+	case scene::ESNAT_FLY_CIRCLE:
+	case scene::ESNAT_FLY_STRAIGHT:
+	case scene::ESNAT_FOLLOW_SPLINE:
+	case scene::ESNAT_ROTATION:
+	case scene::ESNAT_TEXTURE:
+	case scene::ESNAT_DELETION:
+	default:
+		return gcnew SceneNodeAnimator(ref);
+	}
 }
 
 SceneNodeAnimator::SceneNodeAnimator(scene::ISceneNodeAnimator* ref)
