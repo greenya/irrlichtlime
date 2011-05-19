6c67bf0b174fd953df0246c14fab0315ba245ae5
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2011-05-19 11:34:22 +0000

    Added GetItemAt() to GUIListBox.

--- trunk/source/GUIListBox.h
+++ trunk/source/GUIListBox.h
@@ -17,23 +17,33 @@ public:
 
 	int AddItem(String^ text, int icon);
 	int AddItem(String^ text);
+
 	void ClearItemColor(int index, GUIListBoxColor colorType);
 	void ClearItemColors(int index);
 	void ClearItems();
+
 	String^ GetItem(int index);
+	int GetItemAt(int xpos, int ypos);
 	Video::Color^ GetItemDefaultColor(GUIListBoxColor colorType);
 	Video::Color^ GetItemColor(int index, GUIListBoxColor colorType);
 	int GetItemIcon(int index);
+
 	int InsertItem(int index, String^ text, int icon);
 	int InsertItem(int index, String^ text);
+
 	bool ItemColorOverrided(int index, GUIListBoxColor colorType);
+
 	void RemoveItem(int index);
+
 	void SetDrawBackground(bool draw);
+
 	void SetItem(int index, String^ text, int icon);
 	void SetItem(int index, String^ text);
 	void SetItemColor(int index, GUIListBoxColor colorType, Video::Color^ color);
 	void SetItemHeight(int height);
+
 	void SetSpriteBank(GUISpriteBank^ bank);
+
 	void SwapItems(int index1, int index2);
 
 	property bool AutoScroll { bool get(); void set(bool value); }
