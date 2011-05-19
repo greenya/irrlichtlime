504aa1ece185ad8a8457f3fec58aa69ff5ef9a91
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2011-05-19 12:16:02 +0000

    Updated changes.txt. Updated AssemblyInfo version to 0.9.

--- trunk/source/AssemblyInfo.cpp
+++ trunk/source/AssemblyInfo.cpp
@@ -21,7 +21,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("0.8")];
+[assembly:AssemblyVersionAttribute("0.9")];
 
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
