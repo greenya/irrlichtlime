3f0f854835d11766a083d20a993aec6b5b70bb1c
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2011-11-14 23:23:05 +0000

    Added SetShape(), BottomColor, TopColor, BottomWidth, TopWidth, Height and removed Size prop from BillboardSceneNode. 07.Collision example updated.

--- trunk/source/BillboardSceneNode.h
+++ trunk/source/BillboardSceneNode.h
@@ -11,9 +11,20 @@ namespace Scene {
 
 public ref class BillboardSceneNode : SceneNode
 {
+	// Small Notes:
+	// - we use naming "top" (for "top edge" and "end edge") and "bottom" (for "bottom edge" and "start edge", accordingly to native interface) for widths and colors;
+	// - we allow reading and writing single props for better debugging (native interface allows reading and writing by pairs only);
+	// - we replaced Size property with single float Height;
+
 public:
 
-	property Dimension2Df^ Size { Dimension2Df^ get(); void set(Dimension2Df^ value); }
+	void SetShape(float bottomWidth, float topWidth, float height);
+
+	property Video::Color^ BottomColor { Video::Color^ get(); void set(Video::Color^ value); }
+	property float BottomWidth { float get(); void set(float value); }
+	property float Height { float get(); void set(float value); }
+	property Video::Color^ TopColor { Video::Color^ get(); void set(Video::Color^ value); }
+	property float TopWidth { float get(); void set(float value); }
 
 internal:
 
