9d1bd1f0d2e9862c6366aa3c1a98bfe7a20a546f
Yuriy Grynevych <greenyadzer@gmail.com>
2012-08-21 16:03:25 +0000

    Updated changes.txt. Updated AssemblyInfo version to 1.2.

--- source/AssemblyInfo.cpp
+++ source/AssemblyInfo.cpp
@@ -21,7 +21,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.1")];
+[assembly:AssemblyVersionAttribute("1.2")];
 
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
