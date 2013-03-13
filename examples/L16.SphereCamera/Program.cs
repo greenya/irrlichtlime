b1151b158833b57e1185c89299d60d32ee83b432
Yuriy Grynevych <greenyadzer@gmail.com>
2013-03-13 16:24:29 +0000

    Added example L16.SphereCamera.

---
+++ examples/L16.SphereCamera/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using IrrlichtLime;
+using IrrlichtLime.Video;
+using IrrlichtLime.Core;
+using IrrlichtLime.Scene;
+using IrrlichtLime.GUI;
+
+namespace L16.SphereCamera
+{
+	class Program
+	{
+		static IrrlichtDevice device;
+		static VideoDriver driver;
+		static SceneManager scene;
+
+		static MeshSceneNode sphere;
+		static SphereCamera camera;
+		static SpherePath path;
+
+		static void Main(string[] args)
+		{
+			device = IrrlichtDevice.CreateDevice(DriverType.Direct3D9, new Dimension2Di(800, 600));
+			device.SetWindowCaption("Sphere Camera - Irrlicht Engine");
+			driver = device.VideoDriver;
+			scene = device.SceneManager;
+
+			sphere = scene.AddSphereSceneNode(5, 100);
+			sphere.SetMaterialTexture(0, driver.GetTexture("../../media/earth.jpg"));
+			sphere.TriangleSelector = scene.CreateTriangleSelector(sphere.Mesh, sphere);
+			sphere.TriangleSelector.Drop();
+
+			scene.AmbientLight = new Colorf(0.2f, 0.2f, 0.2f);
+			LightSceneNode light = scene.AddLightSceneNode();
+			light.Position = new Vector3Df(-10, 10, -10);
+
+			camera = new SphereCamera(device, new Vector3Df(0), 8, 20, 10, 0, 0);
+			camera.Inclination = 200;
+
+			path = new SpherePath(5.4f);
+
+			GUIFont font = device.GUIEnvironment.BuiltInFont;
+			device.OnEvent += new IrrlichtDevice.EventHandler(device_OnEvent);
+			device.PostEvent(new Event('r', KeyCode.KeyR, true)); // pretend user pressed [R]
+
+			while (device.Run())
+			{
+				driver.BeginScene();
+
+				scene.DrawAll();
+
+				path.Draw(driver);
+
+				font.Draw("Press [Arrows], [LMB] and [Mouse Scroll] to change view", 10, 10, Color.OpaqueYellow);
+				font.Draw("Press [RMB] on Earth to place new path point", 10, 20, Color.OpaqueYellow);
+				font.Draw("Press [R] to reload path data from file", 10, 30, Color.OpaqueYellow);
+				font.Draw("Press [C] to clean up", 10, 40, Color.OpaqueYellow);
+
+				font.Draw(driver.FPS.ToString() + " fps", 10, driver.ScreenSize.Height - 40, Color.OpaqueYellow);
+				font.Draw(path.PointCount.ToString() + " point(s)", 10, driver.ScreenSize.Height - 30, Color.OpaqueYellow);
+				font.Draw(camera.ToString(), 10, driver.ScreenSize.Height - 20, Color.OpaqueYellow);
+
+				driver.EndScene();
+			}
+
+			device.Drop();
+		}
+
+		static bool device_OnEvent(Event evnt)
+		{
+			if (evnt.Type == EventType.Mouse &&
+				evnt.Mouse.Type == MouseEventType.RightDown)
+			{
+				Vector3Df v;
+				Triangle3Df t;
+				SceneNode n;
+
+				Line3Df l = scene.SceneCollisionManager.GetRayFromScreenCoordinates(new Vector2Di(evnt.Mouse.X, evnt.Mouse.Y));
+				if (scene.SceneCollisionManager.GetCollisionPoint(l, sphere.TriangleSelector, out v, out t, out n))
+				{
+					path.AddPoint(v);
+					return true;
+				}
+			}
+
+			if (evnt.Type == EventType.Key)
+			{
+				switch (evnt.Key.Key)
+				{
+					case KeyCode.Left:
+						camera.Inclination++;
+						return true;
+
+					case KeyCode.Right:
+						camera.Inclination--;
+						return true;
+
+					case KeyCode.Up:
+						camera.Azimuth--;
+						return true;
+
+					case KeyCode.Down:
+						camera.Azimuth++;
+						return true;
+
+					case KeyCode.KeyC:
+						path.Clear();
+						return true;
+
+					//case KeyCode.KeyS:
+					//    path.Save("../../media/SphereCameraPath.xml");
+					//    return true;
+
+					case KeyCode.KeyR:
+						path.Load("../../media/SphereCameraPath.xml");
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ examples/L16.SphereCamera/SpherePath.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+using IrrlichtLime.Core;
+using IrrlichtLime.Video;
+using IrrlichtLime.Scene;
+
+namespace L16.SphereCamera
+{
+	class SpherePath
+	{
+		float height;
+		List<Vector3Df> points = new List<Vector3Df>(); // we use this list only to simplify loading and saving routines
+		List<Vertex3D> lineVerticesFront = new List<Vertex3D>();
+		List<Vertex3D> lineVerticesBack = new List<Vertex3D>();
+		List<ushort> lineIndices = new List<ushort>();
+
+		public Vector3Df Center { get; set; }
+		public Color FrontColor { get; set; }
+		public Color BackColor { get; set; }
+
+		public int PointCount
+		{
+			get { return points.Count; }
+		}
+
+		public SpherePath(float height)
+		{
+			this.height = height;
+
+			Center = new Vector3Df(0);
+			FrontColor = Color.OpaqueCyan;
+			BackColor = Color.OpaqueBlue;
+		}
+
+		public void AddPoint(Vector3Df point)
+		{
+			points.Add(point);
+
+			// add front line
+			Vertex3D v1front = new Vertex3D(point, new Vector3Df(0), new Color(0));
+			Vertex3D v2front = new Vertex3D((point - Center).Normalize() * height, new Vector3Df(0), FrontColor);
+			lineVerticesFront.Add(v1front);
+			lineVerticesFront.Add(v2front);
+
+			// add back line
+			Vertex3D v1back = v1front;
+			Vertex3D v2back = new Vertex3D(v2front);
+			v2back.Color = BackColor;
+			lineVerticesBack.Add(v1back);
+			lineVerticesBack.Add(v2back);
+
+			// add indices
+			lineIndices.Add((ushort)lineIndices.Count);
+			lineIndices.Add((ushort)lineIndices.Count);
+
+			// add connection line if possible (front, back and indices)
+			if (lineVerticesFront.Count >= 4)
+			{
+				lineVerticesFront.Add(lineVerticesFront[lineVerticesFront.Count - 3]);
+				lineVerticesFront.Add(v2front);
+				lineVerticesBack.Add(lineVerticesBack[lineVerticesBack.Count - 3]);
+				lineVerticesBack.Add(v2back);
+
+				lineIndices.Add((ushort)lineIndices.Count);
+				lineIndices.Add((ushort)lineIndices.Count);
+			}
+		}
+
+		public void Clear()
+		{
+			points.Clear();
+			lineVerticesFront.Clear();
+			lineVerticesBack.Clear();
+			lineIndices.Clear();
+		}
+
+		public void Draw(VideoDriver driver)
+		{
+			driver.SetTransform(TransformationState.World, Matrix.Identity);
+
+			// set material
+			Material m = Material.IdentityNoLighting;
+			m.Wireframe = true;
+			m.BackfaceCulling = false;
+			m.Type = MaterialType.TransparentVertexAlpha;
+			m.ZWrite = false;
+
+			// draw back lines
+			m.ZBuffer = ComparisonFunc.Greater;
+			driver.SetMaterial(m);
+			driver.DrawVertexPrimitiveList(lineVerticesBack.ToArray(), lineIndices.ToArray(), PrimitiveType.Lines);
+
+			// draw front lines
+			m.ZBuffer = ComparisonFunc.LessEqual;
+			driver.SetMaterial(m);
+			driver.DrawVertexPrimitiveList(lineVerticesFront.ToArray(), lineIndices.ToArray(), PrimitiveType.Lines);
+		}
+
+		public void Save(string filename)
+		{
+			XDocument d = new XDocument();
+			d.Add(new XElement("path"));
+
+			d.Root.Add(new XElement("center", new XAttribute("x", Center.X), new XAttribute("y", Center.Y), new XAttribute("z", Center.Z)));
+			d.Root.Add(new XElement("frontColor", FrontColor.ARGB));
+			d.Root.Add(new XElement("backColor", BackColor.ARGB));
+
+			foreach (Vector3Df p in points)
+				d.Root.Add(new XElement("point", new XAttribute("x", p.X), new XAttribute("y", p.Y), new XAttribute("z", p.Z)));
+
+			d.Save(filename);
+		}
+
+		public void Load(string filename)
+		{
+			Clear();
+
+			XDocument d = XDocument.Load(filename);
+
+			Center.X = Convert.ToSingle(d.Root.Element("center").Attribute("x").Value);
+			Center.Y = Convert.ToSingle(d.Root.Element("center").Attribute("y").Value);
+			Center.Z = Convert.ToSingle(d.Root.Element("center").Attribute("z").Value);
+
+			FrontColor.ARGB = Convert.ToUInt32(d.Root.Element("frontColor").Value);
+			BackColor.ARGB = Convert.ToUInt32(d.Root.Element("backColor").Value);
+
+			foreach (XElement e in d.Root.Elements("point"))
+			{
+				Vector3Df p = new Vector3Df(
+					Convert.ToSingle(e.Attribute("x").Value),
+					Convert.ToSingle(e.Attribute("y").Value),
+					Convert.ToSingle(e.Attribute("z").Value));
+
+				AddPoint(p);
+			}
+		}
+	}
+}
