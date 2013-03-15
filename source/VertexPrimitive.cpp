e915d607f7f73ab5f36173392b7e7a28e00030b2
Yuriy Grynevych <greenyadzer@gmail.com>
2013-03-15 17:13:44 +0000

    Added VertexPrimitive class. This class will provide maximum possible drawing speed using native IVideoDriver::drawVertexPrimitiveList().

---
+++ source/VertexPrimitive.cpp
@@ -0,0 +1,181 @@
+#include "stdafx.h"
+#include "VertexPrimitive.h"
+#include "VideoDriver.h"
+
+using namespace irr;
+using namespace System;
+
+namespace IrrlichtLime {
+namespace Video {
+
+VertexPrimitive::VertexPrimitive(int vertexCount, int indexCount, Scene::PrimitiveType primitiveType)
+{
+	LIME_ASSERT(vertexCount > 0);
+	LIME_ASSERT(indexCount > 0);
+
+	m_primitiveType = (scene::E_PRIMITIVE_TYPE)primitiveType;
+
+	m_primitiveCount = VideoDriver::calculatePrimitiveCount(indexCount, primitiveType);
+
+	m_vertices = new S3DVertex[vertexCount];
+	m_vertexCount = vertexCount;
+
+	m_indices = new u16[indexCount];
+	m_indexCount = indexCount;
+}
+
+void VertexPrimitive::Draw(VideoDriver^ driver)
+{
+	LIME_ASSERT(driver != nullptr);
+	LIME_ASSERT(m_primitiveCount > 0);
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(m_vertexCount > 0);
+	LIME_ASSERT(m_indices != nullptr);
+	LIME_ASSERT(m_indexCount > 0);
+
+	driver->m_VideoDriver->drawVertexPrimitiveList(
+		m_vertices,
+		m_vertexCount,
+		m_indices,
+		m_primitiveCount,
+		video::EVT_STANDARD,
+		m_primitiveType,
+		video::EIT_16BIT);
+}
+
+void VertexPrimitive::Draw(VideoDriver^ driver, unsigned int overridePrimitiveCount)
+{
+	LIME_ASSERT(driver != nullptr);
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(m_vertexCount > 0);
+	LIME_ASSERT(m_indices != nullptr);
+	LIME_ASSERT(m_indexCount > 0);
+
+	if (overridePrimitiveCount == 0)
+		return;
+
+	LIME_ASSERT(m_primitiveCount > 0);
+	LIME_ASSERT(overridePrimitiveCount <= m_primitiveCount);
+
+	driver->m_VideoDriver->drawVertexPrimitiveList(
+		m_vertices,
+		m_vertexCount,
+		m_indices,
+		overridePrimitiveCount,
+		video::EVT_STANDARD,
+		m_primitiveType,
+		video::EIT_16BIT);
+}
+
+void VertexPrimitive::Draw(VideoDriver^ driver, unsigned int overridePrimitiveCount, Scene::PrimitiveType overridePrimitiveType)
+{
+	LIME_ASSERT(driver != nullptr);
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(m_vertexCount > 0);
+	LIME_ASSERT(m_indices != nullptr);
+	LIME_ASSERT(m_indexCount > 0);
+
+	if (overridePrimitiveCount == 0)
+		return;
+
+	unsigned int maxPrimitiveCount = VideoDriver::calculatePrimitiveCount(m_indexCount, overridePrimitiveType);
+
+	LIME_ASSERT(maxPrimitiveCount > 0);
+	LIME_ASSERT(overridePrimitiveCount <= maxPrimitiveCount);
+
+	driver->m_VideoDriver->drawVertexPrimitiveList(
+		m_vertices,
+		m_vertexCount,
+		m_indices,
+		overridePrimitiveCount,
+		video::EVT_STANDARD,
+		(scene::E_PRIMITIVE_TYPE)overridePrimitiveType,
+		video::EIT_16BIT);
+}
+
+void VertexPrimitive::Drop()
+{
+	m_primitiveCount = 0;
+
+	delete[] m_vertices;
+	m_vertices = nullptr;
+	m_vertexCount = 0;
+
+	delete[] m_indices;
+	m_indices = nullptr;
+	m_indexCount = 0;
+}
+
+void VertexPrimitive::SetIndex(int i, int index)
+{
+	LIME_ASSERT(m_indices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_indexCount);
+
+	m_indices[i] = (u16)index;
+}
+
+void VertexPrimitive::SetVertex(int i, Vertex3D^ vertex)
+{
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_vertexCount);
+	LIME_ASSERT(vertex != nullptr);
+
+	m_vertices[i] = *vertex->m_NativeValue;
+}
+
+void VertexPrimitive::SetVertexColor(int i, Color^ color)
+{
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_vertexCount);
+	LIME_ASSERT(color != nullptr);
+
+	m_vertices[i].Color = *color->m_NativeValue;
+}
+
+void VertexPrimitive::SetVertexColor(int i, unsigned int argb)
+{
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_vertexCount);
+
+	m_vertices[i].Color.color = argb;
+}
+
+void VertexPrimitive::SetVertexPosition(int i, Vector3Df^ position)
+{
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_vertexCount);
+	LIME_ASSERT(position != nullptr);
+
+	m_vertices[i].Pos = *position->m_NativeValue;
+}
+
+void VertexPrimitive::SetVertexPosition(int i, float x, float y, float z)
+{
+	LIME_ASSERT(m_vertices != nullptr);
+	LIME_ASSERT(i >= 0 && i < m_vertexCount);
+
+	m_vertices[i].Pos.set(x, y, z);
+}
+
+int VertexPrimitive::IndexCount::get()
+{
+	return m_indexCount;
+}
+
+Scene::PrimitiveType VertexPrimitive::PrimitiveType::get()
+{
+	return (Scene::PrimitiveType)m_primitiveType;
+}
+
+int VertexPrimitive::VertexCount::get()
+{
+	return m_vertexCount;
+}
+
+String^ VertexPrimitive::ToString()
+{
+	return String::Format("VertexPrimitive: PrimitiveType={0}; IndexCount={1}; VertexCount={2}", PrimitiveType, IndexCount, VertexCount);
+}
+
+} // end namespace Video
+} // end namespace IrrlichtLime
---
+++ source/VertexPrimitive.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "stdafx.h"
+
+using namespace irr;
+using namespace System;
+
+namespace IrrlichtLime {
+namespace Video {
+
+public ref class VertexPrimitive
+{
+public:
+
+	VertexPrimitive(int vertexCount, int indexCount, Scene::PrimitiveType primitiveType);
+
+	void Draw(VideoDriver^ driver);
+	void Draw(VideoDriver^ driver, unsigned int overridePrimitiveCount);
+	void Draw(VideoDriver^ driver, unsigned int overridePrimitiveCount, Scene::PrimitiveType overridePrimitiveType);
+
+	void Drop();
+
+	void SetIndex(int i, int index);
+	void SetVertex(int i, Vertex3D^ vertex);
+	void SetVertexColor(int i, Color^ color);
+	void SetVertexColor(int i, unsigned int argb);
+	void SetVertexPosition(int i, Vector3Df^ position);
+	void SetVertexPosition(int i, float x, float y, float z);
+
+	property int IndexCount { int get(); }
+	property Scene::PrimitiveType PrimitiveType { Scene::PrimitiveType get(); }
+	property int VertexCount { int get(); }
+
+	virtual String^ ToString() override;
+
+private:
+
+	scene::E_PRIMITIVE_TYPE m_primitiveType;
+	unsigned int m_primitiveCount;
+
+	video::S3DVertex* m_vertices;
+	int m_vertexCount;
+
+	u16* m_indices;
+	int m_indexCount;
+};
+
+} // end namespace Video
+} // end namespace IrrlichtLime
