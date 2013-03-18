b95fcca71147fb86dfae4aa175d125e5a4ff4350
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2013-03-18 01:27:09 +0000

    Added ability to work with any vertex and index types via VertexPrimitive.

--- trunk/source/VertexPrimitive.cpp
+++ trunk/source/VertexPrimitive.cpp
@@ -8,20 +8,57 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-VertexPrimitive::VertexPrimitive(int vertexCount, int indexCount, Scene::PrimitiveType primitiveType)
+VertexPrimitive::VertexPrimitive(Scene::PrimitiveType primitiveType, Video::VertexType vertexType, int vertexCount, Video::IndexType indexType, int indexCount)
 {
-	LIME_ASSERT(vertexCount > 0 && vertexCount <= 65536);
+	LIME_ASSERT(vertexCount > 0);
 	LIME_ASSERT(indexCount > 0);
 
 	m_primitiveType = (scene::E_PRIMITIVE_TYPE)primitiveType;
+	m_vertexType = (video::E_VERTEX_TYPE)vertexType;
+	m_vertexCount = vertexCount;
+	m_indexType = (video::E_INDEX_TYPE)indexType;
+	m_indexCount = indexCount;
 
 	m_primitiveCount = VideoDriver::calculatePrimitiveCount(indexCount, primitiveType);
 
-	m_vertices = new S3DVertex[vertexCount];
-	m_vertexCount = vertexCount;
+	// allocate vertices
 
-	m_indices = new u16[indexCount];
-	m_indexCount = indexCount;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		m_vertices = new video::S3DVertex[vertexCount];
+		break;
+
+	case video::EVT_2TCOORDS:
+		m_vertices = new video::S3DVertex2TCoords[vertexCount];
+		break;
+
+	case video::EVT_TANGENTS:
+		m_vertices = new video::S3DVertexTangents[vertexCount];
+		break;
+
+	default:
+		LIME_ASSERT2(false, "Unexpected vertexType: " + vertexType.ToString());
+		break;
+	}
+
+	// allocate indices
+
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		LIME_ASSERT(vertexCount <= 65536);
+		m_indices = new u16[indexCount];
+		break;
+
+	case video::EIT_32BIT:
+		m_indices = new u32[indexCount];
+		break;
+
+	default:
+		LIME_ASSERT2(false, "Unexpected indexType: " + indexType.ToString());
+		break;
+	}
 }
 
 void VertexPrimitive::Draw(VideoDriver^ driver)
@@ -38,9 +75,9 @@ void VertexPrimitive::Draw(VideoDriver^ driver)
 		m_vertexCount,
 		m_indices,
 		m_primitiveCount,
-		video::EVT_STANDARD,
+		m_vertexType,
 		m_primitiveType,
-		video::EIT_16BIT);
+		m_indexType);
 }
 
 void VertexPrimitive::Draw(VideoDriver^ driver, int overridePrimitiveCount)
@@ -62,9 +99,9 @@ void VertexPrimitive::Draw(VideoDriver^ driver, int overridePrimitiveCount)
 		m_vertexCount,
 		m_indices,
 		overridePrimitiveCount,
-		video::EVT_STANDARD,
+		m_vertexType,
 		m_primitiveType,
-		video::EIT_16BIT);
+		m_indexType);
 }
 
 void VertexPrimitive::Draw(VideoDriver^ driver, int overridePrimitiveCount, Scene::PrimitiveType overridePrimitiveType)
@@ -79,9 +116,9 @@ void VertexPrimitive::Draw(VideoDriver^ driver, int overridePrimitiveCount, Scen
 		return;
 
 #ifdef _DEBUG
-	int maxPrimitiveCount = VideoDriver::calculatePrimitiveCount(m_indexCount, overridePrimitiveType);
-	LIME_ASSERT(maxPrimitiveCount > 0);
-	LIME_ASSERT(overridePrimitiveCount <= maxPrimitiveCount);
+	int maxPossiblePrimitiveCountWithOverridedType = VideoDriver::calculatePrimitiveCount(m_indexCount, overridePrimitiveType);
+	LIME_ASSERT(maxPossiblePrimitiveCountWithOverridedType > 0);
+	LIME_ASSERT(overridePrimitiveCount <= maxPossiblePrimitiveCountWithOverridedType);
 #endif
 
 	driver->m_VideoDriver->drawVertexPrimitiveList(
@@ -89,20 +126,48 @@ void VertexPrimitive::Draw(VideoDriver^ driver, int overridePrimitiveCount, Scen
 		m_vertexCount,
 		m_indices,
 		overridePrimitiveCount,
-		video::EVT_STANDARD,
+		m_vertexType,
 		(scene::E_PRIMITIVE_TYPE)overridePrimitiveType,
-		video::EIT_16BIT);
+		m_indexType);
 }
 
 void VertexPrimitive::Drop()
 {
 	m_primitiveCount = 0;
 
-	delete[] m_vertices;
+	// delete vertices
+
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		delete[] (video::S3DVertex*)m_vertices;
+		break;
+
+	case video::EVT_2TCOORDS:
+		delete[] (video::S3DVertex2TCoords*)m_vertices;
+		break;
+
+	case video::EVT_TANGENTS:
+		delete[] (video::S3DVertexTangents*)m_vertices;
+		break;
+	}
+
 	m_vertices = nullptr;
 	m_vertexCount = 0;
 
-	delete[] m_indices;
+	// delete indices
+
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		delete[] (u16*)m_indices;
+		break;
+
+	case video::EIT_32BIT:
+		delete[] (u32*)m_indices;
+		break;
+	}
+
 	m_indices = nullptr;
 	m_indexCount = 0;
 }
@@ -112,34 +177,98 @@ int VertexPrimitive::GetIndex(int i)
 	LIME_ASSERT(m_indices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_indexCount);
 
-	return m_indices[i];
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		return (int)(((u16*)m_indices)[i]);
+
+	case video::EIT_32BIT:
+		return (int)(((u32*)m_indices)[i]);
+	}
+
+	return -1;
 }
 
 void VertexPrimitive::SetIndex(int i, int index)
 {
 	LIME_ASSERT(m_indices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_indexCount);
-	LIME_ASSERT(index >= 0 && index <= 65535);
+	LIME_ASSERT(index >= 0);
 
-	m_indices[i] = (u16)index;
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		LIME_ASSERT(index <= 65536);
+		((u16*)m_indices)[i] = (u16)index;
+		return;
+
+	case video::EIT_32BIT:
+		((u32*)m_indices)[i] = (u32)index;
+		return;
+	}
 }
 
-Vertex3D^ VertexPrimitive::GetVertex(int i)
+void VertexPrimitive::SetIndicesSequence()
+{
+	LIME_ASSERT(m_indices != nullptr);
+
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		for (int i = 0; i < m_indexCount; i++)
+			((u16*)m_indices)[i] = (u16)i;
+		return;
+
+	case video::EIT_32BIT:
+		for (int i = 0; i < m_indexCount; i++)
+			((u32*)m_indices)[i] = (u32)i;
+		return;
+	}
+}
+
+Object^ VertexPrimitive::GetVertex(int i)
 {
 	LIME_ASSERT(m_vertices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 
-	Vertex3D^ v = gcnew Vertex3D(m_vertices[i]);
-	return v;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		return gcnew Vertex3D(((video::S3DVertex*)m_vertices)[i]);
+
+	case video::EVT_2TCOORDS:
+		return gcnew Vertex3DTTCoords(((video::S3DVertex2TCoords*)m_vertices)[i]);
+
+	case video::EVT_TANGENTS:
+		return gcnew Vertex3DTangents(((video::S3DVertexTangents*)m_vertices)[i]);
+	}
+
+	return nullptr;
 }
 
-void VertexPrimitive::SetVertex(int i, Vertex3D^ vertex)
+void VertexPrimitive::SetVertex(int i, Object^ vertex)
 {
 	LIME_ASSERT(m_vertices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 	LIME_ASSERT(vertex != nullptr);
 
-	m_vertices[i] = *vertex->m_NativeValue;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		LIME_ASSERT(vertex->GetType() == Vertex3D::typeid);
+		((video::S3DVertex*)m_vertices)[i] = *((Vertex3D^)vertex)->m_NativeValue;
+		return;
+
+	case video::EVT_2TCOORDS:
+		LIME_ASSERT(vertex->GetType() == Vertex3DTTCoords::typeid);
+		((video::S3DVertex2TCoords*)m_vertices)[i] = *((Vertex3DTTCoords^)vertex)->m_NativeValue;
+		return;
+
+	case video::EVT_TANGENTS:
+		LIME_ASSERT(vertex->GetType() == Vertex3DTangents::typeid);
+		((video::S3DVertexTangents*)m_vertices)[i] = *((Vertex3DTangents^)vertex)->m_NativeValue;
+		return;
+	}
 }
 
 void VertexPrimitive::SetVertexColor(int i, Color^ color)
@@ -148,7 +277,20 @@ void VertexPrimitive::SetVertexColor(int i, Color^ color)
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 	LIME_ASSERT(color != nullptr);
 
-	m_vertices[i].Color = *color->m_NativeValue;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		((video::S3DVertex*)m_vertices)[i].Color = *color->m_NativeValue;
+		return;
+
+	case video::EVT_2TCOORDS:
+		((video::S3DVertex2TCoords*)m_vertices)[i].Color = *color->m_NativeValue;
+		return;
+
+	case video::EVT_TANGENTS:
+		((video::S3DVertexTangents*)m_vertices)[i].Color = *color->m_NativeValue;
+		return;
+	}
 }
 
 void VertexPrimitive::SetVertexColor(int i, unsigned int argb)
@@ -156,7 +298,20 @@ void VertexPrimitive::SetVertexColor(int i, unsigned int argb)
 	LIME_ASSERT(m_vertices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 
-	m_vertices[i].Color.color = argb;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		((video::S3DVertex*)m_vertices)[i].Color.color = argb;
+		return;
+
+	case video::EVT_2TCOORDS:
+		((video::S3DVertex2TCoords*)m_vertices)[i].Color.color = argb;
+		return;
+
+	case video::EVT_TANGENTS:
+		((video::S3DVertexTangents*)m_vertices)[i].Color.color = argb;
+		return;
+	}
 }
 
 void VertexPrimitive::SetVertexPosition(int i, Vector3Df^ position)
@@ -165,7 +320,20 @@ void VertexPrimitive::SetVertexPosition(int i, Vector3Df^ position)
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 	LIME_ASSERT(position != nullptr);
 
-	m_vertices[i].Pos = *position->m_NativeValue;
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		((video::S3DVertex*)m_vertices)[i].Pos = *position->m_NativeValue;
+		return;
+
+	case video::EVT_2TCOORDS:
+		((video::S3DVertex2TCoords*)m_vertices)[i].Pos = *position->m_NativeValue;
+		return;
+
+	case video::EVT_TANGENTS:
+		((video::S3DVertexTangents*)m_vertices)[i].Pos = *position->m_NativeValue;
+		return;
+	}
 }
 
 void VertexPrimitive::SetVertexPosition(int i, float x, float y, float z)
@@ -173,7 +341,20 @@ void VertexPrimitive::SetVertexPosition(int i, float x, float y, float z)
 	LIME_ASSERT(m_vertices != nullptr);
 	LIME_ASSERT(i >= 0 && i < m_vertexCount);
 
-	m_vertices[i].Pos.set(x, y, z);
+	switch (m_vertexType)
+	{
+	case video::EVT_STANDARD:
+		((video::S3DVertex*)m_vertices)[i].Pos.set(x, y, z);
+		return;
+
+	case video::EVT_2TCOORDS:
+		((video::S3DVertex2TCoords*)m_vertices)[i].Pos.set(x, y, z);
+		return;
+
+	case video::EVT_TANGENTS:
+		((video::S3DVertexTangents*)m_vertices)[i].Pos.set(x, y, z);
+		return;
+	}
 }
 
 int VertexPrimitive::IndexCount::get()
@@ -181,9 +362,43 @@ int VertexPrimitive::IndexCount::get()
 	return m_indexCount;
 }
 
+Video::IndexType VertexPrimitive::IndexType::get()
+{
+	return (Video::IndexType)m_indexType;
+}
+
+int VertexPrimitive::PrimitiveCount::get()
+{
+	return m_primitiveCount;
+}
+
 Scene::PrimitiveType VertexPrimitive::PrimitiveType::get()
 {
 	return (Scene::PrimitiveType)m_primitiveType;
+}
+
+unsigned long VertexPrimitive::TotalBytesAllocated::get()
+{
+	unsigned long t = 0;
+
+	switch (m_indexType)
+	{
+	case video::EIT_16BIT:
+		t += ((unsigned long)m_indexCount) * 2;
+		break;
+
+	case video::EIT_32BIT:
+		t += ((unsigned long)m_indexCount) * 4;
+		break;
+
+	default:
+		LIME_ASSERT2(false, "Unexpected IndexType: " + IndexType.ToString());
+		break;
+	}
+
+	t += ((unsigned long)m_vertexCount) * video::getVertexPitchFromType(m_vertexType);
+
+	return t;
 }
 
 int VertexPrimitive::VertexCount::get()
@@ -191,9 +406,16 @@ int VertexPrimitive::VertexCount::get()
 	return m_vertexCount;
 }
 
+Video::VertexType VertexPrimitive::VertexType::get()
+{
+	return (Video::VertexType)m_vertexType;
+}
+
 String^ VertexPrimitive::ToString()
 {
-	return String::Format("VertexPrimitive: PrimitiveType={0}; IndexCount={1}; VertexCount={2}", PrimitiveType, IndexCount, VertexCount);
+	return String::Format(
+		"VertexPrimitive: PrimitiveType={0}; PrimitiveCount={1}; IndexCount={2}; VertexCount={3}",
+		PrimitiveType, PrimitiveCount, IndexCount, VertexCount);
 }
 
 } // end namespace Video
