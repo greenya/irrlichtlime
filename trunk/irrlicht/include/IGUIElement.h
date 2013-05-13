f752240049610715abba4bfdbee60976bb451572
greenya <greenya@8967289b-0294-4328-a5d5-493833e94945>
2013-05-13 15:57:36 +0000

    Updated Irrlicht SDK to trunk rev. 4515.

--- trunk/irrlicht/include/IGUIElement.h
+++ trunk/irrlicht/include/IGUIElement.h
@@ -346,6 +346,20 @@ public:
 		return IsVisible;
 	}
 
+	//! Check whether the element is truly visible, taking into accounts its parents' visibility
+	/** \return true if the element and all its parents are visible,
+	false if this or any parent element is invisible. */
+	virtual bool isTrulyVisible() const
+	{
+		_IRR_IMPLEMENT_MANAGED_MARSHALLING_BUGFIX;
+		if(!IsVisible)
+			return false;
+
+		if(!Parent)
+			return true;
+
+		return Parent->isTrulyVisible();
+	}
 
 	//! Sets the visible state of this element.
 	virtual void setVisible(bool visible)
@@ -550,7 +564,7 @@ public:
 	virtual bool sendToBack(IGUIElement* child)
 	{
 		core::list<IGUIElement*>::Iterator it = Children.begin();
-		if (child == (*it))	// already there
+		if (child == (*it)) // already there
 			return true;
 		for (; it != Children.end(); ++it)
 		{
@@ -734,7 +748,7 @@ public:
 	{
 		return GUIElementTypeNames[Type];
 	}
-	
+
 	//! Returns the name of the element.
 	/** \return Name as character string. */
 	virtual const c8* getName() const
@@ -764,7 +778,7 @@ public:
 	scripting languages, editors, debuggers or xml serialization purposes. */
 	virtual void serializeAttributes(io::IAttributes* out, io::SAttributeReadWriteOptions* options=0) const
 	{
-		out->addString("Name", Name.c_str());		
+		out->addString("Name", Name.c_str());
 		out->addInt("Id", ID );
 		out->addString("Caption", getText());
 		out->addRect("Rect", DesiredRect);
@@ -1003,7 +1017,7 @@ protected:
 
 	//! tooltip
 	core::stringw ToolTipText;
-	
+
 	//! users can set this for identificating the element by string
 	core::stringc Name;
 
--- trunk/irrlicht/include/IrrCompileConfig.h
+++ trunk/irrlicht/include/IrrCompileConfig.h
@@ -7,12 +7,12 @@
 
 //! Irrlicht SDK Version
 #define IRRLICHT_VERSION_MAJOR 1
-#define IRRLICHT_VERSION_MINOR 8
+#define IRRLICHT_VERSION_MINOR 9
 #define IRRLICHT_VERSION_REVISION 0
 // This flag will be defined only in SVN, the official release code will have
 // it undefined
-//#define IRRLICHT_VERSION_SVN -alpha
-#define IRRLICHT_SDK_VERSION "1.8.0"
+#define IRRLICHT_VERSION_SVN alpha
+#define IRRLICHT_SDK_VERSION "1.9.0"
 
 #include <stdio.h> // TODO: Although included elsewhere this is required at least for mingw
 
@@ -158,6 +158,10 @@ headers, e.g. Summer 2004.  This is a Microsoft issue, not an Irrlicht one.
 If not defined, Windows Multimedia library is used, which offers also broad support for joystick devices. */
 #define _IRR_COMPILE_WITH_DIRECTINPUT_JOYSTICK_
 #ifdef NO_IRR_COMPILE_WITH_DIRECTINPUT_JOYSTICK_
+#undef _IRR_COMPILE_WITH_DIRECTINPUT_JOYSTICK_
+#endif
+// can't get this to compile currently under borland, can be removed if someone has a better solution
+#if defined(__BORLANDC__)
 #undef _IRR_COMPILE_WITH_DIRECTINPUT_JOYSTICK_
 #endif
 
@@ -387,7 +391,7 @@ B3D, MS3D or X meshes */
 #ifdef NO_IRR_COMPILE_WITH_OGRE_LOADER_
 #undef _IRR_COMPILE_WITH_OGRE_LOADER_
 #endif
-#endif	// _IRR_COMPILE_WITH_SKINNED_MESH_SUPPORT_
+#endif // _IRR_COMPILE_WITH_SKINNED_MESH_SUPPORT_
 
 //! Define _IRR_COMPILE_WITH_IRR_MESH_LOADER_ if you want to load Irrlicht Engine .irrmesh files
 #define _IRR_COMPILE_WITH_IRR_MESH_LOADER_
@@ -532,14 +536,24 @@ B3D, MS3D or X meshes */
 #ifdef NO_IRR_COMPILE_WITH_PSD_LOADER_
 #undef _IRR_COMPILE_WITH_PSD_LOADER_
 #endif
-//! Define _IRR_COMPILE_WITH_DDS_LOADER_ if you want to load .dds files
+//! Define _IRR_COMPILE_WITH_DDS_LOADER_ if you want to load compressed .dds files
+// Patent problem isn't related to this loader.
+#define _IRR_COMPILE_WITH_DDS_LOADER_
+#ifdef NO_IRR_COMPILE_WITH_DDS_LOADER_
+#undef _IRR_COMPILE_WITH_DDS_LOADER_
+#endif
+//! Define _IRR_COMPILE_WITH_DDS_DECODER_LOADER_ if you want to load .dds files
+//! loader will decompress these files and will send to the memory as uncompressed files.
 // Outcommented because
 // a) it doesn't compile on 64-bit currently
 // b) anyone enabling it should be aware that S3TC compression algorithm which might be used in that loader
 // is patented in the US by S3 and they do collect license fees when it's used in applications.
 // So if you are unfortunate enough to develop applications for US market and their broken patent system be careful.
-// #define _IRR_COMPILE_WITH_DDS_LOADER_
-#ifdef NO_IRR_COMPILE_WITH_DDS_LOADER_
+// #define _IRR_COMPILE_WITH_DDS_DECODER_LOADER_
+#ifdef NO_IRR_COMPILE_WITH_DDS_DECODER_LOADER_
+#undef _IRR_COMPILE_WITH_DDS_DECODER_LOADER_
+#endif
+#ifdef _IRR_COMPILE_WITH_DDS_DECODER_LOADER_
 #undef _IRR_COMPILE_WITH_DDS_LOADER_
 #endif
 //! Define _IRR_COMPILE_WITH_TGA_LOADER_ if you want to load .tga files
@@ -814,6 +828,23 @@ precision will be lower but speed higher. currently X86 only
 	#undef _IRR_COMPILE_WITH_IRR_WRITER_
 	#undef _IRR_COMPILE_WITH_COLLADA_WRITER_
 	#undef _IRR_COMPILE_WITH_COLLADA_LOADER_
+#endif
+
+#if defined(__BORLANDC__)
+	#include <tchar.h>
+
+	// Borland 5.5.1 does not have _strcmpi defined
+	#if __BORLANDC__ == 0x551
+	//    #define _strcmpi strcmpi
+		#undef _tfinddata_t
+		#undef _tfindfirst
+		#undef _tfindnext
+
+		#define _tfinddata_t __tfinddata_t
+		#define _tfindfirst  __tfindfirst
+		#define _tfindnext   __tfindnext
+		typedef long intptr_t;
+	#endif
 #endif
 
 #endif // __IRR_COMPILE_CONFIG_H_INCLUDED__
