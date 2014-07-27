f26f38c90a40b6617da4820e68dd86cb9add0cce
f-o-a-l-y <f-o-a-l-y@8967289b-0294-4328-a5d5-493833e94945>
2014-07-27 13:18:14 +0000

    -Class ReadFileStream added: Allows .Net like reading from ReadFiles. Can be used with XMLReaders, etc.

---
+++ trunk/source/ReadFileStream.cpp
@@ -0,0 +1,113 @@
+#include "stdafx.h"
+#include "ReadFileStream.h"
+#include "IReadFile.h"
+#include "ReadFile.h"
+
+using namespace irr;
+using namespace System;
+using namespace System::IO;
+
+namespace IrrlichtLime {
+namespace IO {
+
+ReadFileStream::ReadFileStream()
+{
+	m_ReadFile = nullptr;
+}
+
+ReadFileStream::ReadFileStream(ReadFile^ readFile)
+{
+	m_ReadFile = LIME_SAFEREF(readFile, m_ReadFile);
+	if (m_ReadFile != nullptr)
+		m_ReadFile->grab();
+}
+
+bool ReadFileStream::CanRead::get()
+{
+	if (m_ReadFile == nullptr)
+		return false;
+	return m_ReadFile->getPos() < m_ReadFile->getSize();
+}
+
+bool ReadFileStream::CanWrite::get()
+{
+	return false;
+}
+
+bool ReadFileStream::CanSeek::get()
+{
+	if (m_ReadFile == nullptr)
+		return false;
+	return true;
+}
+
+long long ReadFileStream::Length::get()
+{
+	if (m_ReadFile == nullptr)
+		throw gcnew InvalidOperationException("File is null!");
+	return m_ReadFile->getSize();
+}
+
+long long ReadFileStream::Position::get()
+{
+	if (m_ReadFile == nullptr)
+		throw gcnew InvalidOperationException("File is null!");
+	return m_ReadFile->getPos();
+}
+
+void ReadFileStream::Position::set(long long value)
+{
+	if (m_ReadFile == nullptr)
+		throw gcnew InvalidOperationException("File is null!");
+	m_ReadFile->seek(value);
+}
+
+ReadFile^ ReadFileStream::File::get()
+{
+	return ReadFile::Wrap(m_ReadFile);
+}
+
+void ReadFileStream::File::set(ReadFile^ value)
+{
+	if (m_ReadFile != nullptr)
+		m_ReadFile->drop();
+	m_ReadFile = LIME_SAFEREF(value, m_ReadFile);
+	if (m_ReadFile != nullptr)
+		m_ReadFile->grab();
+}
+
+long long ReadFileStream::Seek(long long offset, SeekOrigin origin)
+{
+	if (m_ReadFile == nullptr)
+		throw gcnew InvalidOperationException("File is null!");
+	switch (origin)
+    {
+	case SeekOrigin::Begin:
+		m_ReadFile->seek((int)offset);
+		break;
+	case SeekOrigin::Current:
+		m_ReadFile->seek((int)offset, true);
+		break;
+	case SeekOrigin::End:
+		m_ReadFile->seek((int)(offset + (long)m_ReadFile->getSize()));
+		break;
+    }
+    return Position;
+}
+
+int ReadFileStream::Read(array<Byte>^ buffer, int offset, int count)
+{
+	if (m_ReadFile == nullptr)
+		throw gcnew InvalidOperationException("File is null!");
+
+	if (Position == Length || count + offset > buffer->Length)
+		return 0;
+
+	pin_ptr<Byte> bufferPtr = &buffer[0];
+	Byte* offsettedPtr = &bufferPtr[offset];
+	int s = m_ReadFile->read(offsettedPtr, count);
+	return s;
+}
+
+} // end namespace IO
+} // end namespace IrrlichtLime
---
+++ trunk/source/ReadFileStream.h
@@ -0,0 +1,61 @@
+#pragma once
+
+using namespace irr;
+using namespace System;
+using namespace System::IO;
+
+namespace IrrlichtLime {
+namespace IO {
+
+ref class ReadFile;
+
+public ref class ReadFileStream : Stream
+{
+public:
+
+	ReadFileStream();
+	ReadFileStream(ReadFile^ readFile);
+
+	//Properties
+	property bool CanRead { virtual bool get() override; }
+	property bool CanWrite { virtual bool get() override; }
+	property bool CanSeek { virtual bool get() override; }
+	property long long Length { virtual long long get() override; }
+	property long long Position { virtual long long get() override; virtual void set(long long value) override; }
+	property ReadFile^ File { ReadFile^ get(); void set(ReadFile^ value); }
+
+#pragma warning (push)
+#pragma warning (disable: 4100)
+	virtual void SetLength(long long value) override { }	//Not implemented
+#pragma warning (pop)
+	virtual long long Seek(long long offset, SeekOrigin origin) override;
+
+	virtual int Read(array<Byte>^ buffer, int offset, int count) override;
+#pragma warning (push)
+#pragma warning (disable: 4100)
+	virtual void Write(array<Byte>^ buffer, int offset, int count) override { throw gcnew InvalidOperationException(); }
+#pragma warning (pop)
+	virtual void Flush() override { }	//Not implemented
+
+	~ReadFileStream()
+	{
+		this->!ReadFileStream();
+	}
+
+	!ReadFileStream()
+	{
+		if (m_ReadFile != nullptr)
+		{
+			m_ReadFile->drop();
+			m_ReadFile = nullptr;
+		}
+	}
+
+private:
+
+	io::IReadFile* m_ReadFile;
+
+};
+
+} // end namespace IO
+} // end namespace IrrlichtLime
