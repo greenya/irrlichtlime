a1dbdd35d011f8ad48df7f2b586c985e7d9e4477
greenya <greenyadzer@gmail.com>
2019-06-04 00:00:03 +0300

    Update GUITableDrawFlag

--- source/GUITableDrawFlag.h
+++ source/GUITableDrawFlag.h
@@ -9,13 +9,27 @@ using namespace System;
 namespace IrrlichtLime {
 namespace GUI {
 
-[Flags]
-public enum class GUITableDrawFlag
-{
-	Rows = EGTDF_ROWS,
-	Columns = EGTDF_COLUMNS,
-	ActiveRow = EGTDF_ACTIVE_ROW
-};
+	[Flags]
+	/// <summary>
+	/// GUI table draw flags.
+	/// </summary>
+	public enum class GUITableDrawFlag
+	{
+		/// <summary>
+		/// Draw rows.
+		/// </summary>
+		Rows = EGTDF_ROWS,
+
+		/// <summary>
+		/// Draw columns.
+		/// </summary>
+		Columns = EGTDF_COLUMNS,
+
+		/// <summary>
+		/// Draw active row.
+		/// </summary>
+		ActiveRow = EGTDF_ACTIVE_ROW
+	};
 
 } // end namespace GUI
 } // end namespace IrrlichtLime
