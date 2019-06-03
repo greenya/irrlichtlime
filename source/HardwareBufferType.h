f7fc217b8e3bfcc47b3b2019196d315bb258248b
greenya <greenyadzer@gmail.com>
2019-06-04 00:04:37 +0300

    Update doc for HardwareBufferType

--- source/HardwareBufferType.h
+++ source/HardwareBufferType.h
@@ -9,13 +9,31 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Scene {
 
-public enum class HardwareBufferType
-{
-	None = EBT_NONE,
-	Vertex = EBT_VERTEX,
-	Index = EBT_INDEX,
-	VertexAndIndex = EBT_VERTEX_AND_INDEX
-};
+	/// <summary>
+	/// Hardware buffer types.
+	/// </summary>
+	public enum class HardwareBufferType
+	{
+		/// <summary>
+		/// Does not change anything.
+		/// </summary>
+		None = EBT_NONE,
+
+		/// <summary>
+		/// Change the vertex mapping.
+		/// </summary>
+		Vertex = EBT_VERTEX,
+
+		/// <summary>
+		/// Change the index mapping.
+		/// </summary>
+		Index = EBT_INDEX,
+
+		/// <summary>
+		/// Change both vertex and index mapping to the same value.
+		/// </summary>
+		VertexAndIndex = EBT_VERTEX_AND_INDEX
+	};
 
 } // end namespace Scene
 } // end namespace IrrlichtLime
