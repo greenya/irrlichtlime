a381744269340ef9e749c66c5ff548a6db9c88ba
greenya <greenyadzer@gmail.com>
2019-06-03 23:23:59 +0300

    Update GUIMessageBoxFlag

--- source/GUIMessageBoxFlag.h
+++ source/GUIMessageBoxFlag.h
@@ -9,14 +9,32 @@ using namespace System;
 namespace IrrlichtLime {
 namespace GUI {
 
-[Flags]
-public enum class GUIMessageBoxFlag
-{
-	OK = EMBF_OK,
-	Cancel = EMBF_CANCEL,
-	Yes = EMBF_YES,
-	No = EMBF_NO
-};
+	[Flags]
+	/// <summary>
+	/// Enumeration for message box layout flags.
+	/// </summary>
+	public enum class GUIMessageBoxFlag
+	{
+		/// <summary>
+		/// Flag for the OK button.
+		/// </summary>
+		OK = EMBF_OK,
+
+		/// <summary>
+		/// Flag for the cancel button.
+		/// </summary>
+		Cancel = EMBF_CANCEL,
+
+		/// <summary>
+		/// Flag for the yes button.
+		/// </summary>
+		Yes = EMBF_YES,
+
+		/// <summary>
+		/// Flag for the no button.
+		/// </summary>
+		No = EMBF_NO
+	};
 
 } // end namespace GUI
 } // end namespace IrrlichtLime
