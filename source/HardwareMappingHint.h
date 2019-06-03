fad47c99a50122537779b1a951d9153f42a565a3
greenya <greenyadzer@gmail.com>
2019-06-04 00:08:59 +0300

    Update doc for HardwareMappingHint

--- source/HardwareMappingHint.h
+++ source/HardwareMappingHint.h
@@ -9,13 +9,31 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Scene {
 
-public enum class HardwareMappingHint
-{
-	Never = EHM_NEVER,
-	Static = EHM_STATIC,
-	Dynamic = EHM_DYNAMIC,
-	Stream = EHM_STREAM
-};
+	/// <summary>
+	/// Hardware mapping hints.
+	/// </summary>
+	public enum class HardwareMappingHint
+	{
+		/// <summary>
+		/// Don't store on the hardware.
+		/// </summary>
+		Never = EHM_NEVER,
+
+		/// <summary>
+		/// Rarely changed, usually stored completely on the hardware.
+		/// </summary>
+		Static = EHM_STATIC,
+
+		/// <summary>
+		/// Sometimes changed, driver optimized placement.
+		/// </summary>
+		Dynamic = EHM_DYNAMIC,
+
+		/// <summary>
+		/// Always changed, cache optimizing on the GPU.
+		/// </summary>
+		Stream = EHM_STREAM
+	};
 
 } // end namespace Scene
 } // end namespace IrrlichtLime
