47c02be29dab3ba486d1f06fdc91d2a1b7908396
greenya <greenyadzer@gmail.com>
2019-06-04 00:14:29 +0300

    Update doc for JointUpdateOnRender

--- source/JointUpdateOnRender.h
+++ source/JointUpdateOnRender.h
@@ -8,12 +8,26 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Scene {
 
-public enum class JointUpdateOnRender
-{
-	None = EJUOR_NONE,
-	Read = EJUOR_READ,
-	Control = EJUOR_CONTROL
-};
+	/// <summary>
+	/// Joint update modes.
+	/// </summary>
+	public enum class JointUpdateOnRender
+	{
+		/// <summary>
+		/// Do nothing.
+		/// </summary>
+		None = EJUOR_NONE,
+
+		/// <summary>
+		/// Get joints positions from the mesh (for attached nodes, etc).
+		/// </summary>
+		Read = EJUOR_READ,
+
+		/// <summary>
+		/// Control joint positions in the mesh (e.g. ragdolls, or set the animation from <c>AnimateJoints()</c>).
+		/// </summary>
+		Control = EJUOR_CONTROL
+	};
 
 } // end namespace Scene
 } // end namespace IrrlichtLime
