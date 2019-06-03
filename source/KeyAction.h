2f1cfd3e542b79ff8760656eec57ed1fb2100710
greenya <greenyadzer@gmail.com>
2019-06-04 00:19:10 +0300

    Update KeyAction with latest API

--- source/KeyAction.h
+++ source/KeyAction.h
@@ -7,14 +7,50 @@ using namespace System;
 
 namespace IrrlichtLime {
 
-public enum class KeyAction
-{
-	MoveForward = EKA_MOVE_FORWARD,
-	MoveBackward = EKA_MOVE_BACKWARD,
-	StrafeLeft = EKA_STRAFE_LEFT,
-	StrafeRight = EKA_STRAFE_RIGHT,
-	Jump = EKA_JUMP_UP,
-	Crouch = EKA_CROUCH
-};
+	/// <summary>
+	/// Enumeration for key actions. Used for example in the FPS Camera.
+	/// </summary>
+	public enum class KeyAction
+	{
+		/// <summary>
+		/// Move forward.
+		/// </summary>
+		MoveForward = EKA_MOVE_FORWARD,
+
+		/// <summary>
+		/// Move backward.
+		/// </summary>
+		MoveBackward = EKA_MOVE_BACKWARD,
+
+		/// <summary>
+		/// Strafe left.
+		/// </summary>
+		StrafeLeft = EKA_STRAFE_LEFT,
+
+		/// <summary>
+		/// Strafe right.
+		/// </summary>
+		StrafeRight = EKA_STRAFE_RIGHT,
+
+		/// <summary>
+		/// Jump.
+		/// </summary>
+		Jump = EKA_JUMP_UP,
+
+		/// <summary>
+		/// Crouch.
+		/// </summary>
+		Crouch = EKA_CROUCH,
+
+		/// <summary>
+		/// Rotate left.
+		/// </summary>
+		RotateLeft = EKA_ROTATE_LEFT,
+
+		/// <summary>
+		/// Rotate right.
+		/// </summary>
+		RotateRight = EKA_ROTATE_RIGHT
+	};
 
 } // end namespace IrrlichtLime
