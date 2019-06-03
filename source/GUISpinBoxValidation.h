8c0e846dd876748eb7ce3da43bbf9a060da064fc
greenya <greenyadzer@gmail.com>
2019-06-03 23:57:49 +0300

    Update GUISpinBoxValidation

--- source/GUISpinBoxValidation.h
+++ source/GUISpinBoxValidation.h
@@ -9,14 +9,33 @@ using namespace System;
 namespace IrrlichtLime {
 namespace GUI {
 
-[Flags]
-public enum class GUISpinBoxValidation
-{
-	Never = EGUI_SBV_NEVER,
-	Change = EGUI_SBV_CHANGE,
-	Enter = EGUI_SBV_ENTER,
-	LoseFocus = EGUI_SBV_LOSE_FOCUS
-};
+	[Flags]
+	/// <summary>
+	/// Enumeration bitflag for when to validate the text typed into the spinbox.
+	/// Default used by Irrlicht is: <see cref="GUISpinBoxValidation.Enter"/> | <see cref="GUISpinBoxValidation.LoseFocus"/>)
+	/// </summary>
+	public enum class GUISpinBoxValidation
+	{
+		/// <summary>
+		/// Does not validate typed text, probably a bad idea setting this usually.
+		/// </summary>
+		Never = EGUI_SBV_NEVER,
+
+		/// <summary>
+		/// Validate on each change. Was default up to Irrlicht 1.8.
+		/// </summary>
+		Change = EGUI_SBV_CHANGE,
+
+		/// <summary>
+		/// Validate when enter was pressed.
+		/// </summary>
+		Enter = EGUI_SBV_ENTER,
+
+		/// <summary>
+		/// Validate when the editbox loses the focus.
+		/// </summary>
+		LoseFocus = EGUI_SBV_LOSE_FOCUS
+	};
 
 } // end namespace GUI
 } // end namespace IrrlichtLime
