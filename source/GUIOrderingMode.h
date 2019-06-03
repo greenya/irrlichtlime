05a9c42afbb94253fae299dae102e147dc148608
greenya <greenyadzer@gmail.com>
2019-06-03 23:28:52 +0300

    Update GUIOrderingMode

--- source/GUIOrderingMode.h
+++ source/GUIOrderingMode.h
@@ -9,12 +9,26 @@ using namespace System;
 namespace IrrlichtLime {
 namespace GUI {
 
-public enum class GUIOrderingMode
-{
-	None = EGOM_NONE,
-	Ascending = EGOM_ASCENDING,
-	Descending = EGOM_DESCENDING
-};
+	/// <summary>
+	/// Ordering modes.
+	/// </summary>
+	public enum class GUIOrderingMode
+	{
+		/// <summary>
+		/// No element ordering.
+		/// </summary>
+		None = EGOM_NONE,
+
+		/// <summary>
+		/// Elements are ordered from the smallest to the largest.
+		/// </summary>
+		Ascending = EGOM_ASCENDING,
+
+		/// <summary>
+		/// Elements are ordered from the largest to the smallest.
+		/// </summary>
+		Descending = EGOM_DESCENDING
+	};
 
 } // end namespace GUI
 } // end namespace IrrlichtLime
