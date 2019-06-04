0209fdfeb4ec2131ea1b9f4a66e8033d900abfd6
greenya <greenyadzer@gmail.com>
2019-06-05 02:04:44 +0300

    Update doc for TransformationState

--- source/TransformationState.h
+++ source/TransformationState.h
@@ -9,28 +9,79 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-public enum class TransformationState
-{
-	View = ETS_VIEW,
-	World = ETS_WORLD,
-	Projection = ETS_PROJECTION,
-	Texture0 = ETS_TEXTURE_0,
-	Texture1 = ETS_TEXTURE_1,
-	Texture2 = ETS_TEXTURE_2,
-	Texture3 = ETS_TEXTURE_3,
+	/// <summary>
+	/// Enumeration for geometry transformation states.
+	/// </summary>
+	public enum class TransformationState
+	{
+		/// <summary>
+		/// View transformation.
+		/// </summary>
+		View = ETS_VIEW,
+
+		/// <summary>
+		/// World transformation.
+		/// </summary>
+		World = ETS_WORLD,
+
+		/// <summary>
+		/// Projection transformation.
+		/// </summary>
+		Projection = ETS_PROJECTION,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture0 = ETS_TEXTURE_0,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture1 = ETS_TEXTURE_1,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture2 = ETS_TEXTURE_2,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture3 = ETS_TEXTURE_3,
+
 #if _IRR_MATERIAL_MAX_TEXTURES_>4
-	Texture4 = ETS_TEXTURE_4,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture4 = ETS_TEXTURE_4,
+
 #if _IRR_MATERIAL_MAX_TEXTURES_>5
-	Texture5 = ETS_TEXTURE_5,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture5 = ETS_TEXTURE_5,
+
 #if _IRR_MATERIAL_MAX_TEXTURES_>6
-	Texture6 = ETS_TEXTURE_6,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture6 = ETS_TEXTURE_6,
+
 #if _IRR_MATERIAL_MAX_TEXTURES_>7
-	Texture7 = ETS_TEXTURE_7,
+
+		/// <summary>
+		/// Texture transformation.
+		/// </summary>
+		Texture7 = ETS_TEXTURE_7,
+
 #endif
 #endif
 #endif
 #endif
-};
+	};
 
 } // end namespace Video
 } // end namespace IrrlichtLime
