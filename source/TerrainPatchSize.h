155bf4637d0b54199f508cee4d599a6c303bb22c
greenya <greenyadzer@gmail.com>
2019-06-05 01:23:35 +0300

    Update doc for TerrainPatchSize

--- source/TerrainPatchSize.h
+++ source/TerrainPatchSize.h
@@ -9,14 +9,36 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Scene {
 
-public enum class TerrainPatchSize
-{
-	_9 = ETPS_9,
-	_17 = ETPS_17,
-	_33 = ETPS_33,
-	_65 = ETPS_65,
-	_129 = ETPS_129
-};
+	/// <summary>
+	/// Enumeration for patch sizes specifying the size of patches in the <c>TerrainSceneNode</c>.
+	/// </summary>
+	public enum class TerrainPatchSize
+	{
+		/// <summary>
+		/// Patch size of 9, at most, use 4 levels of detail with this patch size.
+		/// </summary>
+		_9 = ETPS_9,
+
+		/// <summary>
+		/// Patch size of 17, at most, use 5 levels of detail with this patch size.
+		/// </summary>
+		_17 = ETPS_17,
+
+		/// <summary>
+		/// Patch size of 33, at most, use 6 levels of detail with this patch size.
+		/// </summary>
+		_33 = ETPS_33,
+
+		/// <summary>
+		/// Patch size of 65, at most, use 7 levels of detail with this patch size.
+		/// </summary>
+		_65 = ETPS_65,
+
+		/// <summary>
+		/// Patch size of 129, at most, use 8 levels of detail with this patch size.
+		/// </summary>
+		_129 = ETPS_129
+	};
 
 } // end namespace Scene
 } // end namespace IrrlichtLime
