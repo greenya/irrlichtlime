dc6bf99d3d2a9b3a47df02bcd52ee9588b7e6332
greenya <greenyadzer@gmail.com>
2019-06-05 01:26:52 +0300

    Update doc for TextureClamp

--- source/TextureClamp.h
+++ source/TextureClamp.h
@@ -9,17 +9,51 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-public enum class TextureClamp
-{
-	Repeat = ETC_REPEAT,
-	Clamp = ETC_CLAMP,
-	ClampToEdge = ETC_CLAMP_TO_EDGE,
-	ClampToBorder = ETC_CLAMP_TO_BORDER,
-	Mirror = ETC_MIRROR,
-	MirrorClamp = ETC_MIRROR_CLAMP,
-	MirrorClampToEdge = ETC_MIRROR_CLAMP_TO_EDGE,
-	MirrorClampToBorder = ETC_MIRROR_CLAMP_TO_BORDER
-};
+	/// <summary>
+	/// Texture coord clamp mode outside [0.0, 1.0].
+	/// </summary>
+	public enum class TextureClamp
+	{
+		/// <summary>
+		/// Texture repeats.
+		/// </summary>
+		Repeat = ETC_REPEAT,
+
+		/// <summary>
+		/// Texture is clamped to the last pixel.
+		/// </summary>
+		Clamp = ETC_CLAMP,
+
+		/// <summary>
+		/// Texture is clamped to the edge pixel.
+		/// </summary>
+		ClampToEdge = ETC_CLAMP_TO_EDGE,
+
+		/// <summary>
+		/// Texture is clamped to the border pixel (if exists).
+		/// </summary>
+		ClampToBorder = ETC_CLAMP_TO_BORDER,
+
+		/// <summary>
+		/// Texture is alternatingly mirrored (0..1..0..1..0..).
+		/// </summary>
+		Mirror = ETC_MIRROR,
+
+		/// <summary>
+		/// Texture is mirrored once and then clamped (0..1..0).
+		/// </summary>
+		MirrorClamp = ETC_MIRROR_CLAMP,
+
+		/// <summary>
+		/// Texture is mirrored once and then clamped to edge.
+		/// </summary>
+		MirrorClampToEdge = ETC_MIRROR_CLAMP_TO_EDGE,
+
+		/// <summary>
+		/// Texture is mirrored once and then clamped to border.
+		/// </summary>
+		MirrorClampToBorder = ETC_MIRROR_CLAMP_TO_BORDER
+	};
 
 } // end namespace Video
 } // end namespace IrrlichtLime
