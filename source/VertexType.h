5a679c9c24d78d0a26ec8bd5c1f4a097c9ce190f
greenya <greenyadzer@gmail.com>
2019-06-05 02:12:12 +0300

    Update doc for VertexType

--- source/VertexType.h
+++ source/VertexType.h
@@ -9,12 +9,29 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-public enum class VertexType
-{
-	Standard = EVT_STANDARD,
-	TTCoords = EVT_2TCOORDS,
-	Tangents = EVT_TANGENTS
-};
+	/// <summary>
+	/// Enumeration for all vertex types there are.
+	/// </summary>
+	public enum class VertexType
+	{
+		/// <summary>
+		/// Standard vertex type used by the Irrlicht engine (<c>Vertex3D</c>).
+		/// </summary>
+		Standard = EVT_STANDARD,
+
+		/// <summary>
+		/// Vertex with two texture coordinates (<c>Vertex3DTTCoords</c>).
+		/// Usually used for geometry with lightmaps or other special materials.
+		/// </summary>
+		TTCoords = EVT_2TCOORDS,
+
+		/// <summary>
+		/// Vertex with a tangent and binormal vector (<c>Vertex3DTangents</c>).
+		/// Usually used for tangent space normal mapping.
+		/// Usually tangent and binormal get send to shaders as texture coordinate sets 1 and 2.
+		/// </summary>
+		Tangents = EVT_TANGENTS
+	};
 
 } // end namespace Video
 } // end namespace IrrlichtLime
