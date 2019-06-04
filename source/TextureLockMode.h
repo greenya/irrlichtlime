3f84becc7275e79386b182ea044506ab4e1399ec
greenya <greenyadzer@gmail.com>
2019-06-05 01:46:34 +0300

    Update doc for TextureLockMode

--- source/TextureLockMode.h
+++ source/TextureLockMode.h
@@ -9,12 +9,29 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-public enum class TextureLockMode
-{
-	ReadWrite = ETLM_READ_WRITE,
-	ReadOnly = ETLM_READ_ONLY,
-	WriteOnly = ETLM_WRITE_ONLY
-};
+	/// <summary>
+	/// Enumeration for the mode for texture locking.
+	/// </summary>
+	public enum class TextureLockMode
+	{
+		/// <summary>
+		/// The default mode. Texture can be read and written to.
+		/// </summary>
+		ReadWrite = ETLM_READ_WRITE,
+
+		/// <summary>
+		/// Read only. The texture is downloaded, but not uploaded again.
+		/// Often used to read back shader generated textures.
+		/// </summary>
+		ReadOnly = ETLM_READ_ONLY,
+
+		/// <summary>
+		/// Write only. The texture is not downloaded and might be uninitialized.
+		/// The updated texture is uploaded to the GPU.
+		/// Used for initializing the shader from the CPU.
+		/// </summary>
+		WriteOnly = ETLM_WRITE_ONLY
+	};
 
 } // end namespace Video
 } // end namespace IrrlichtLime
