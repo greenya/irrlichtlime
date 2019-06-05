b1995cae5664a6d4ad56d37409f48468787cc7ef
greenya <greenyadzer@gmail.com>
2019-06-05 03:19:43 +0300

    Update doc for VideoModeAspectRatio

--- source/VideoModeAspectRatio.h
+++ source/VideoModeAspectRatio.h
@@ -9,16 +9,47 @@ using namespace System;
 namespace IrrlichtLime {
 namespace Video {
 
-public enum class VideoModeAspectRatio
-{
-	Other,
-	_3x2,
-	_4x3,
-	_5x3,
-	_5x4,
-	_16x9,
-	_16x10
-};
+	/// <summary>
+	/// Video aspect ratios.
+	/// Use <c>VideoMode.GetAspectRatio()</c> to get the value.
+	/// </summary>
+	public enum class VideoModeAspectRatio
+	{
+		/// <summary>
+		/// Other aspect ratio.
+		/// </summary>
+		Other,
+
+		/// <summary>
+		/// 3x2 aspect ratio.
+		/// </summary>
+		_3x2,
+
+		/// <summary>
+		/// 4x3 aspect ratio.
+		/// </summary>
+		_4x3,
+
+		/// <summary>
+		/// 5x3 aspect ratio.
+		/// </summary>
+		_5x3,
+
+		/// <summary>
+		/// 5x4 aspect ratio.
+		/// </summary>
+		_5x4,
+
+		/// <summary>
+		/// 16x9 aspect ratio.
+		/// </summary>
+		_16x9,
+
+		/// <summary>
+		/// 16x10 aspect ratio.
+		/// </summary>
+		_16x10
+	};
 
 } // end namespace Video
 } // end namespace IrrlichtLime
